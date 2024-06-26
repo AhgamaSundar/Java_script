1a7f7b8751fc2e4883219de66565245259f962c5
AhgamaSundar <ahgamasundar40@gmail.com>
2024-06-26 20:06:50 +0530

    Added loop test

---
+++ conditional.js
@@ -0,0 +1,5 @@
+let name ="Ahgama"
+if(name=="Ahgama") {
+    console.log("unlucky");
+
+}
--- newapp.js
+++ newapp.js
@@ -1,16 +1,24 @@
-let inp=document.getElementById("Product")
-let para =document.querySelector("p");
-let cols=document.getElementById("update");
-cols.classList.add("warning");
+let inp = document.getElementById("Product");
+let para = document.querySelector("p");
+let cols = document.getElementById("update");
 
-function update(event){
-    let number=event.target.value;
-    let len=number.length;
-   
-    para.innerHTML=inp.maxLength-len+"/60"
-     
+function update(event) {
+  let number = event.target.value;
+  let len = number.length;
+  let remlen = inp.maxLength - len;
+  cols.innerHTML = remlen;
+  if (remlen <= 10) {
+    inp.classList.add("warning");
+    cols.classList.add("warning");
+  }else if(remlen==0){
+    inp.classList.add("turnr");
+
+  }
+   else {
+    inp.classList.remove("warning","tunr");
+    cols.classList.remove("warning");
     
-     
-    
+  }
 }
-inp.addEventListener('input',update)
+
+inp.addEventListener("input", update);
