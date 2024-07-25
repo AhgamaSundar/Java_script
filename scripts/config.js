3aa29b54007f97dad950803ad4fbbe4ef5c2392e
AhgamaSundar <ahgamasundar40@gmail.com>
2024-07-25 19:22:31 +0530

    added functionality to edit button

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,17 @@
 function openPlayerConfig(){
     playerConfigOverlay.style.display="block";
     backdrop.style.display="block";
+}
+
+function closePlayerConfig(){
+    playerConfigOverlay.style.display="none";
+    backdrop.style.display="none";
+}
+
+function savePlayerConfig(event){
+    event.preventDefault();
+    const fromData=new FormData(event.target);
+    const enteredPlayername=fromData.get('username');
+    console.log(enteredPlayername);
 
 }
