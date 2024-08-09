32a36f57bfa4b88c46deb3fd0ec24609fbfeebb1
AhgamaSundar <ahgamasundar40@gmail.com>
2024-08-09 19:44:44 +0530

    added the user input configs

--- scripts/config.js
+++ scripts/config.js
@@ -6,12 +6,27 @@ function openPlayerConfig(){
 function closePlayerConfig(){
     playerConfigOverlay.style.display="none";
     backdrop.style.display="none";
+    formElement.firstElementChild.classList.remove("error");
+
 }
 
 function savePlayerConfig(event){
     event.preventDefault();
     const fromData=new FormData(event.target);
-    const enteredPlayername=fromData.get('username');
-    console.log(enteredPlayername);
+    const enteredPlayername=fromData.get('username').trim();
+    document.querySelector("form div input").addEventListener('click', clickontext);
+
+    if (!enteredPlayername){
+        event.target.firstElementChild.classList.add("error");
+        errorsOutputElement.textContent="Please Enter a name valid name!";
+        return;
+
+    }
+}
+function clickontext(){
+    formElement.firstElementChild.classList.remove("error");
+    errorsOutputElement.textContent="";
+
 
 }
+
